c384affe0ac303bb1a468721d4a0daf743115303
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-02 18:45:09 +0200

    user login init

--- client/my-app/src/components/createUserForm.js
+++ client/my-app/src/components/createUserForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import LoginForm from "./loginForm";
 import {
   FaRegEnvelope,
@@ -10,6 +10,41 @@ import {
 import { Link, Route, Routes } from "react-router-dom";
 
 export default function ContactForm() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [createAccountComplite, setAccountComplite] = useState(null);
+
+  useEffect(() => {
+    if (userName.length < 5 && password.length < 8) {
+      setAccountComplite(false);
+    } else {
+      setAccountComplite(true);
+    }
+  }, [userName, password]);
+
+  const CheckUser = () => {
+    console.log(userName.length);
+    if (userName.length > 5 && password.length > 8) {
+      console.log("check user info complited");
+
+      setAccountComplite(true);
+    } else {
+      setAccountComplite(false);
+      console.log("check user info is not complited");
+    }
+  };
+
+  const HandlerValueName = (e) => {
+    let newNameValue = e.target.value;
+
+    setUserName(newNameValue);
+  };
+
+  const HandlerValuePassword = (e) => {
+    let newPasswordValue = e.target.value;
+    setPassword(newPasswordValue);
+  };
+
   return (
     <>
       <div className="form-conatiner">
@@ -26,17 +61,30 @@ export default function ContactForm() {
             </button>
           </section>
 
-          <div className="inputs-box">
+          <div className={`inputs-box`}>
             <label htmlFor="email">{<FaRegEnvelope />} Username</label>
-            <input className="form-item" name="email" id="email" type="email" />
+            <input
+              value={userName}
+              onChange={(e) => HandlerValueName(e)}
+              className="form-item"
+              name="email"
+              id="email"
+              type="email"
+            />
             <label htmlFor="password">{<FaUnlock />} Password</label>
             <input
               className="form-item"
               name="password"
               id="password"
               type="password"
+              onChange={(e) => HandlerValuePassword(e)}
+              value={password}
             />
-            <button className="form-item" id="btn-form">
+            <button
+              onClick={(e) => CheckUser(e)}
+              className="form-item"
+              id="btn-form"
+            >
               CREATE
             </button>
 
