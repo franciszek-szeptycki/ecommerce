3af132da1a280eb513076addea98e084a08bb8fb
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-04 13:21:48 +0200

    "createUserForm" - code review

--- client/my-app/src/components/accountsDataComponents/createUserForm.js
+++ client/my-app/src/components/accountsDataComponents/createUserForm.js
@@ -12,41 +12,18 @@ import { Link, Route, Routes } from "react-router-dom";
 export default function ContactForm() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [newPasswordComplite, setNewPasswordComplite] = useState("Password");
-  const [newUsernameComplite, setNewUsernameComplite] = useState("Username");
 
-  const CheckPassword = () => {
-    if (password.length < 8) {
-      setNewPasswordComplite("Password must be has min 8 tags");
-    } else {
-      setNewPasswordComplite("Password");
-    }
-  };
-
-  const CheckName = () => {
-    if (userName.length < 3) {
-      setNewUsernameComplite("Your name must be min 3 tags !!!");
-    } else {
-      setNewUsernameComplite("Username");
-    }
+  const createNewAccount = () => {
+    console.log("user is login !");
   };
 
   const CheckUser = (e) => {
     e.preventDefault();
-    console.log(userName);
-    console.log(password);
-    CheckPassword();
-    CheckName();
-  };
 
-  const inputsHandler = (event) => {
-    const target = event.target;
-    let newValue = event.target.value;
-
-    if (target.id === "email") {
-      setUserName(newValue);
-    } else if (target.id === "password") {
-      setPassword(newValue);
+    if (userName.length > 2 && password.length > 7) {
+      createNewAccount();
+    } else {
+      alert("Your name or password is to short !");
     }
   };
 
@@ -69,24 +46,22 @@ export default function ContactForm() {
           <div className={`inputs-box `}>
             <label htmlFor="email">
               {<FaRegEnvelope />}
-              {newUsernameComplite}
+              Username
             </label>
             <input
               value={userName}
-              onChange={(e) => inputsHandler(e)}
+              onChange={(e) => setUserName(e.target.value)}
               className="form-item"
               name="email"
               id="email"
             />
-            <label htmlFor="password">
-              {<FaUnlock />} {newPasswordComplite}
-            </label>
+            <label htmlFor="password">{<FaUnlock />} Password</label>
             <input
               className="form-item"
               name="password"
               id="password"
               type="password"
-              onChange={(e) => inputsHandler(e)}
+              onChange={(e) => setPassword(e.target.value)}
               value={password}
             />
             <button
