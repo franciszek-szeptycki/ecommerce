985bfbe6d48ea94eb5cf1e8b523ad5a6dce38a9a
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-05 17:28:00 +0200

    responsive user menu

--- client/my-app/src/components/mainAppLayout/layoutComponents/navBar.js
+++ client/my-app/src/components/mainAppLayout/layoutComponents/navBar.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function NavBar() {
+  const [menuHandler, setmenuHandler] = useState(false);
+
   return (
-    <nav className="navigation">
-      <h3>ECOMMERCE</h3>
+    <>
+      <nav className="navigation">
+        <h3>ECOMMERCE</h3>
 
-      <ul>
-        <li>About</li>
-        <li>Colaboration</li>
-        <li>Help</li>
-      </ul>
+        <ul>
+          <li>About</li>
+          <li>Colaboration</li>
+          <li>Help</li>
+        </ul>
 
-      <button className="btn-contact-nav">Contact</button>
+        <button className="btn-contact-nav">Contact</button>
 
-      <button id="btn-toggle">{<FaBars />}</button>
-    </nav>
+        <button onClick={() => setmenuHandler(!menuHandler)} id="btn-toggle">
+          {<FaBars />}
+        </button>
+      </nav>
+      <div className={`drop-menu ${menuHandler ? "active-menu" : ""}`}>
+        <header>
+          <button className="btn-close-menu">Contact</button>
+        </header>
+        <ul>
+          <li>About</li>
+          <li>Colaboration</li>
+          <li>Help</li>
+        </ul>
+      </div>
+    </>
   );
 }
