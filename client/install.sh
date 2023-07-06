8d69c54625f17a3146dac7e23bdac6543f768482
franciszek-szeptycki-wise-people <f.szeptycki@itiswise.com>
2023-07-06 21:23:31 +0200

    vite config

---
+++ client/install.sh
@@ -0,0 +1,9 @@
+cd vite-project
+npm install
+npm run build
+
+cd ..
+rm -rf vite-project
+
+cd proxy
+npm install
--- client/proxy/index.js
+++ client/proxy/index.js
@@ -4,29 +4,27 @@ const httpProxy = require('http-proxy')
 const app = express()
 const proxy = httpProxy.createProxyServer()
 
-const { CLIENT_PORT, SERVER_PORT, SERVER_CONTAINER_NAME } = process.env
 const STATIC_DIR = 'public'
+const PORT = 3000
+const SERVER_PORT = 8000
+const SERVER_HOST = 'server'
 
-PROXY_CONFIG = {
-    target: `http://${SERVER_CONTAINER_NAME}:${SERVER_PORT}`,
-}
+const PROXY = { target: `http://${SERVER_HOST}:${SERVER_PORT}` }
 
 app.use(express.static(STATIC_DIR))
 
 app.all('/api/*', (req, res) => {
-    console.log(PROXY_CONFIG)
-    proxy.web(req, res, PROXY_CONFIG)
+    proxy.web(req, res, PROXY)
 })
 
 app.all('/auth/*', (req, res) => {
-    console.log(PROXY_CONFIG)
-    proxy.web(req, res, PROXY_CONFIG)
+    proxy.web(req, res, PROXY)
 })
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/${STATIC_DIR}/index.html`)
 })
 
-app.listen(CLIENT_PORT, () => {
-    console.log(`Server is running on port ${CLIENT_PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
