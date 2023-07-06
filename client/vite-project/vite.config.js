b1e5963a1c59e9c5c4f35528f49c1ee0a29b6393
franciszek-szeptycki-wise-people <f.szeptycki@itiswise.com>
2023-07-06 21:23:31 +0200

    vite config

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -3,5 +3,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+
+    build: {
+        outDir: '../proxy/public',
+        emptyOutDir: true,
+    },
+
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                // changeOrigin: true,
+            },
+            '/auth': {
+                target: 'http://127.0.0.1:8000',
+                // changeOrigin: true,
+            },
+        },
+    },
 })
