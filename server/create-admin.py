b7b91595f17d15111542b7b87d171a0a66c5b8b2
franciszek-szeptycki-wise-people <f.szeptycki@itiswise.com>
2023-07-08 18:22:53 +0200

    auto admin

---
+++ server/create-admin.py
@@ -0,0 +1,25 @@
+import os
+import django
+
+# Ustawienia środowiska Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nazwa_projektu.settings")
+django.setup()
+
+from django.contrib.auth.models import User
+
+def create_admin():
+    username = 'admin'
+    password = 'admin123'
+    email = 'admin@example.com'
+
+    if User.objects.filter(username=username).exists():
+        print('Konto administratora już istnieje.')
+        return
+
+    # Tworzenie konta administratora
+    admin = User.objects.create_superuser(username=username, email=email, password=password)
+    print('Konto administratora zostało utworzone.')
+
+
+if __name__ == '__main__':
+    create_admin()
--- server/run-server.sh
+++ server/run-server.sh
@@ -5,5 +5,7 @@ python manage.py migrate
 
 # python manage.py collectstatic --noinput
 
+python create_admin.py
+
 gunicorn --bind 0.0.0.0:8000 app.wsgi:application
 # python manage.py runserver
