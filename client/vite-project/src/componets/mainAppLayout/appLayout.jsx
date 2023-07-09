bd4278141603990ed1ac28e4ee8c738124882ce5
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-09 11:36:00 +0200

    navigation correct

--- client/vite-project/src/componets/mainAppLayout/appLayout.jsx
+++ client/vite-project/src/componets/mainAppLayout/appLayout.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import NavBar from "./layoutComponents/navBar";
+
 import "./mainStyles/mainAppStyles.css";
 
 export default function AppLayout() {
-  return <NavBar />;
+  
+  return (
+    <>
+    <NavBar/>
+  
+    </>
+  );
 }
--- client/vite-project/src/componets/mainAppLayout/layoutComponents/navBar.jsx
+++ client/vite-project/src/componets/mainAppLayout/layoutComponents/navBar.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 
+//Componetns import
+import LoginForm from "../../accountsDataComponents/loginForm";
+import CreateUserAccountForm from "../../accountsDataComponents/createUserForm";
+
+//Image icons import
+import Icon from "./images/user.png";
+
+//Icons import
 import {
   FaBars,
   FaRegLightbulb,
   FaUserCircle,
   FaRegEdit,
-  FaShoppingCart
+  FaShoppingCart,
+  FaCheckCircle,
+  FaTimes,
 } from "react-icons/fa";
+
+//Router import
+
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 
 export default function NavBar() {
   const [menuHandler, setmenuHandler] = useState(false);
+  const [modalState, setModalState] = useState(false);
 
   return (
     <>
@@ -17,20 +32,23 @@ export default function NavBar() {
         <h3>ECOMMERCE</h3>
 
         <ul>
-          <li>{<FaUserCircle />}   About</li>
-          <li>{<FaRegEdit />}   Colaboration</li>
-          <li>{<FaRegLightbulb />}   Help</li>
+          <li>{<FaUserCircle />} About</li>
+          <li>{<FaRegEdit />} Colaboration</li>
+          <li>{<FaRegLightbulb />} Help</li>
         </ul>
 
         <button className="btn-contact-nav">Contact</button>
         <div className="wrapper-options">
-        <ul id="options-list">
-          <li>{<FaUserCircle/>}</li>
-          <li>{<FaShoppingCart/>}</li>
-        </ul>
-      
+          <ul id="options-list">
+            <li
+              className="options-list-item"
+              onClick={() => setModalState(true)}
+            >
+              {<FaUserCircle />}
+            </li>
+            <li className="options-list-item">{<FaShoppingCart />}</li>
+          </ul>
         </div>
-       
 
         <button onClick={() => setmenuHandler(!menuHandler)} id="btn-toggle">
           {<FaBars />}
@@ -44,9 +62,47 @@ export default function NavBar() {
           <li>{<FaUserCircle />} About</li>
           <li>{<FaRegEdit />} Colaboration</li>
           <li>{<FaRegLightbulb />} Help</li>
+          <li className="options-list-item" onClick={() => setModalState(true)}>
+            {<FaUserCircle />} Account
+          </li>
+          <li className="options-list-item">{<FaShoppingCart />} My orders</li>
         </ul>
       </div>
-     
+
+      <div
+        className={`modal-main-container ${modalState ? "active-modal" : ""} `}
+      >
+        <div className="modal-container">
+          <header>
+            <img src={Icon} alt="" />
+            <button
+              className="btn-hidden-modal"
+              onClick={() => setModalState(false)}
+            >
+              {<FaTimes />}
+            </button>
+          </header>
+          <section className="modal-text-descryption-container">
+            <p>
+              Hi, there! you can choose whether you want to log in or create an
+              account or not
+            </p>
+          </section>
+          <section className="options-container-modal">
+            <Link to="/login">
+              <button>Login {<FaCheckCircle />}</button>
+            </Link>
+            <Link to="/register">
+              <button>Register {<FaCheckCircle />}</button>
+            </Link>
+          </section>
+        </div>
+      </div>
+
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<CreateUserAccountForm />} />
+      </Routes>
     </>
   );
 }
