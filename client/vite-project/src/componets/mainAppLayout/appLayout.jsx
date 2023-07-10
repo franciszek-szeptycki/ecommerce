2da2b8c2fbf6d2b3ab9ade91d7083ccaa7f0ba24
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-10 12:27:57 +0200

    Install Axios

--- client/vite-project/src/componets/mainAppLayout/appLayout.jsx
+++ client/vite-project/src/componets/mainAppLayout/appLayout.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import NavBar from "./layoutComponents/navBar";
-
+import axios from 'axios'
 import "./mainStyles/mainAppStyles.css";
 
 export default function AppLayout() {
 
-
+  window.onload = () => {
+    //axios.get('')
+  }
   return (
     <>
     <NavBar/>
