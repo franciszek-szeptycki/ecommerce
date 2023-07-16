c838cf68209b4c15cb04abe3f58ea1c2218bd353
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-16 13:32:16 +0200

    .

--- client/vite-project/src/componets/mainAppLayout/layoutComponents/ProductsLayout/ProductsBoard.jsx
+++ client/vite-project/src/componets/mainAppLayout/layoutComponents/ProductsLayout/ProductsBoard.jsx
@@ -3,6 +3,7 @@ import ProductsItem from "./ProductsItem";
 export default function ProductsBoard(props) {
   const [products, setProducts] = useState([]);
   //https://fer1s.github.io/
+  //
   const productsElementsMap = products.map((element) => {
     return <ProductsItem element={element} />;
   });
