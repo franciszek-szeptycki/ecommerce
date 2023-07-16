02cdd3aa35a97452634e22403c6438f9c3b70229
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-16 15:00:22 +0200

    Categories data fetch

--- client/vite-project/src/componets/mainAppLayout/layoutComponents/ProductsLayout/Category.jsx
+++ client/vite-project/src/componets/mainAppLayout/layoutComponents/ProductsLayout/Category.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Category(props){
     return(
-        <li>{}</li>
+        <li key={props.element.id}>{props.element.name}</li>
     )
 }
--- client/vite-project/src/componets/mainAppLayout/layoutComponents/ProductsLayout/ProductsSideMenu.jsx
+++ client/vite-project/src/componets/mainAppLayout/layoutComponents/ProductsLayout/ProductsSideMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Category from "./Category";
 import axios from "axios";
 import "./styles/ProductsMain.css";
@@ -6,12 +6,20 @@ import { FaTimes } from "react-icons/fa";
 export default function ProductsSideMenu(props) {
   const [categorys, setCategorys] = useState([]);
 
-  window.onload = () => {
-    axios.get("/api/products/").then((res) => {
-      //setCategorys(categorys.push(res.data));
-      console.log(res);
-    });
-  };
+   
+   const getDataCategories = () => {
+    console.log("test")
+
+    axios.get('/api/categories/').then((res) => {
+     
+      setCategorys(res.data)
+      
+    })
+   } 
+
+ 
+
+   useEffect(() => {getDataCategories()}, [])
 
   const categorysElementsMap = categorys.map((element, idx) => {
     return <Category element={element} />;
